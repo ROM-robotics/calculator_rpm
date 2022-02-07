28f7a71fdb42b093113d9eea1458be7eb9d032b1
ROM Robotics <server02.psa1981@gmail.com>
2022-02-07 21:16:39 +0630

    Update counter

--- calculator_ticks_counter/calculator_ticks_counter.ino
+++ calculator_ticks_counter/calculator_ticks_counter.ino
@@ -8,12 +8,17 @@
 #include <Wire.h>
 #include "robot_specs.h"
 
-#define right_encoderA      19     // (interrupt numbers 0)
-#define right_encoderB      18    // (interrupt numbers 1)
+//#define right_encoderA      19     // (interrupt numbers 0)       // sittwe left
+//#define right_encoderB      18     // (interrupt numbers 1)
+//#define ENA1 5 
+//#define Right_in1 6
+//#define Right_in2 7
 
-#define ENA1 5
-#define Right_in1 6
-#define Right_in2 7
+#define right_encoderA      3                                       // sittwe right
+#define right_encoderB      2
+//#define ENA1 10             
+//#define Right_in1 9
+//#define Right_in2 8
 
 volatile long right_count = 0;
 
